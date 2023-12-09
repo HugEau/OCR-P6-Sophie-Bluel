ce8b7af6fca24f593647bd151dbfd87bb12d052e
HugEau <hugo260704chesnel@gmail.com>
2023-12-10 00:31:55 +0100

    Updated modal feature
    Finished modal delete part (JS => updated feature by fetching API rather than reading html code)
    
    Created modal add part (JS & CSS)

--- FrontEnd/assets/script/loggedIndex.js
+++ FrontEnd/assets/script/loggedIndex.js
@@ -1,3 +1,22 @@
+// Function to fetch data from API
+async function loggedFetchData() {
+    try {
+        // Fetch categories and works data from API
+        let categoriesResponse = await fetch("http://localhost:5678/api/categories");
+        let worksResponse = await fetch("http://localhost:5678/api/works");
+
+        // Convert response data to JSON
+        let categories = await categoriesResponse.json();
+        let works = await worksResponse.json();
+        
+        modalContentAdd(works)
+
+    } catch (error) {
+        // Handle any errors that occur during fetching
+        console.error('Error fetching data:', error);
+    }
+}
+
 const modalContainer = document.querySelector(".modalContainer")
 const connectionCookie = document.cookie
 const portfolioBtn = document.querySelector(".portfolio__header")
@@ -34,7 +53,7 @@ function modifyBtnCreate() {
 
     //while using modify btn => show modal menu
     modifyBtn.addEventListener('click', () => {
-        modalContentAdd();
+        loggedFetchData();
         // Add feature to show modal menu
         modalContainer.style.display = "flex";
     })
@@ -43,57 +62,26 @@ function modifyBtnCreate() {
 function modalCreate() {
     //create modal menu to manage works
     let modalCreate = document.createElement("div")
+    modalCreate.className = "connect modal"
+    
+    //create modal Header
     let modalHeaderCreate = document.createElement("div")
+    modalHeaderCreate.className = "modalHeader"
 
+    //create modal close feature
     let modalClose = document.createElement("button")
     modalClose.className = "connect modalBtnClose"
     modalClose.innerHTML = '<i class="fa-solid fa-xmark fa-xl"></i>'
     modalHeaderCreate.appendChild(modalClose)
 
+    //create modalBody
     let modalBodyCreate = document.createElement("div")
+    modalBodyCreate.className = "modalBody"
 
+    //create modal txt presentation feature
     let modalBodyTxtCtn = document.createElement("div")
     modalBodyTxtCtn.className = "connect modalBodyTxtCtn"
     modalBodyCreate.appendChild(modalBodyTxtCtn)
-
-    let modalBodyGallery = document.createElement("div")
-    modalBodyGallery.className = "connect modalBodyGallery"
-    modalBodyCreate.appendChild(modalBodyGallery)
-
-    let modalFooterCreate = document.createElement("div")
-    
-    modalHeaderCreate.className = "modalHeader"
-
-    modalBodyCreate.className = "modalBody"
-
-
-    modalFooterCreate.className = "modalFooter"
-
-
-    modalCreate.className = "connect modal"
-
-    modalCreate.appendChild(modalHeaderCreate)
-    modalCreate.appendChild(modalBodyCreate)
-    modalCreate.appendChild(modalFooterCreate)
-    modalContainer.appendChild(modalCreate)
-
-    //Close modal feature
-    modalContainer.addEventListener("click:not(.modal)", () => {
-        modalContainer.style.display = "none";
-    })
-
-    modalClose.addEventListener("click", () => {
-        modalContainer.style.display = "none";
-    })
-}
-
-function modalContentAdd() {
-    // Add modal content
-
-    let modalGallery = document.querySelector(".modalBodyGallery");
-    let modalExistingWorks = modalGallery.querySelectorAll("figure");
-    let modalWorks = gallery.querySelectorAll("figure");
-    let modalBodyTxtCtn = document.querySelector(".modalBodyTxtCtn")
 
     if (!document.contains(document.querySelector(".modalBodyTxt"))) {
         let modalBodyTxt = document.createElement("h2")
@@ -101,37 +89,218 @@ function modalContentAdd() {
         modalBodyTxt.innerHTML = "Galerie photo"
 
         modalBodyTxtCtn.appendChild(modalBodyTxt)
-    }  
+    } else {
+        let modalBodyTxt = document.querySelector(".modalBodyTxt")
+        modalBodyTxt.innerHTML = "Galerie photo" 
+    }
+
+    //create image container feature
+    let modalBodyGallery = document.createElement("div")
+    modalBodyGallery.className = "connect modalBodyGallery"
+    modalBodyCreate.appendChild(modalBodyGallery)
+
+    //create add feature
+    let categoriesArray = [
+        {
+            "name": "Objet" ,
+            "id": "1",
+        },
+        {
+            "name": "Appartements",
+            "id": "2"
+        },
+        {
+            "name": "Hôtels & Restaurants",
+            "id": "3"
+        }
+        
+    ]
+    let modalBodyAddDiv = document.createElement("div")
+    modalBodyAddDiv.className = "connect modalBodyAddDiv"
+    
+        //create image add feature
+        // A VOIR MODIFIER BTN IMG ADD => INPUT IMAGE
+        let modalImgAddDiv = document.createElement("div")
+        modalImgAddDiv.className = "connect modalImgAddDiv"
+
+        let modalImgAddIconCtn = document.createElement("div")
+        modalImgAddIconCtn.className = "connect modalImgAddIconCtn"
+        modalImgAddDiv.appendChild(modalImgAddIconCtn)
+        
+        let modalImgAddIcon = document.createElement("i")
+        modalImgAddIcon.className = "fa-regular fa-image fa-2xl"
+        modalImgAddIconCtn.appendChild(modalImgAddIcon)
+        modalImgAddDiv.appendChild(modalImgAddIconCtn)
+
+        let modalImgAddBtn = document.createElement("button")
+        modalImgAddBtn.className = "connect modalImgAddBtn"
+        modalImgAddBtn.innerHTML = "+ Ajouter photo"
+        modalImgAddDiv.appendChild(modalImgAddBtn)
+
+        let modalImgAddTxt = document.createElement("p")
+        modalImgAddTxt.className = "connect modalImgAddTxt"
+        modalImgAddTxt.innerHTML = "jpg, png : 4mo max"
+        modalImgAddDiv.appendChild(modalImgAddTxt)
+
+    modalBodyAddDiv.appendChild(modalImgAddDiv)
+        //Create form feature
+        let modalAddFormDiv = document.createElement("div")
+        modalAddFormDiv.className = "connect modalAddFormDiv"
+
+        let modalAddForm = document.createElement("form")
+        modalAddForm.method = "post"
+
+        let modalAddFormTitleTxt = document.createElement("label")
+        modalAddFormTitleTxt.for = "title"
+        modalAddFormTitleTxt.innerHTML = "Titre"
+        modalAddForm.appendChild(modalAddFormTitleTxt)
+
+        let modalAddFormTitleInput = document.createElement("input")
+        modalAddFormTitleInput.name = "title"
+        modalAddFormTitleInput.id = "title"
+        modalAddFormTitleInput.type = "text"
+        modalAddForm.appendChild(modalAddFormTitleInput)
+
+        let modalAddFormCategoryTxt = document.createElement("label")
+        modalAddFormCategoryTxt.for = "categorySelector"
+        modalAddFormCategoryTxt.innerHTML = "Catégorie"
+        modalAddForm.appendChild(modalAddFormCategoryTxt)
+
+        let modalAddFormCategorySelect = document.createElement("select")
+        modalAddFormCategorySelect.name = "categorySelector"
+        modalAddFormCategorySelect.id = "categorySelector"
+        modalAddForm.appendChild(modalAddFormCategorySelect)
+        
+        let modalAddFormCategoryOption = document.createElement("option")
+        modalAddFormCategoryOption.value = "Sélectonnez une catégorie de travail"
+        modalAddFormCategoryOption.innerHTML = ""
+        modalAddFormCategorySelect.appendChild(modalAddFormCategoryOption)
+        for (let i = 0; i < categoriesArray.length; i++) {
+            let modalAddFormCategoryOption = document.createElement("option")
+            modalAddFormCategoryOption.value = categoriesArray[i].name
+            modalAddFormCategoryOption.innerHTML = categoriesArray[i].name
+            modalAddFormCategoryOption.id = categoriesArray[i].id
+            modalAddFormCategorySelect.appendChild(modalAddFormCategoryOption)
+        }
+        modalAddFormDiv.appendChild(modalAddForm)
+    modalBodyAddDiv.appendChild(modalAddFormDiv)
+    modalBodyCreate.appendChild(modalBodyAddDiv)
+
+    //create modal footer
+    let modalFooterCreate = document.createElement("div")
+    modalFooterCreate.className = "modalFooter"
+
+    //Initiate modal creation
+    modalCreate.appendChild(modalHeaderCreate)
+    modalCreate.appendChild(modalBodyCreate)
+    modalCreate.appendChild(modalFooterCreate)
+    modalContainer.appendChild(modalCreate)
+
+    //Close modal function feature + Refresh page to show modifications
+    modalClose.addEventListener("click", () => {
+        
+        modalContainer.style.display = "none";
+        modalBodyAddDiv.style.display = "none"
+        modalBodyGallery.style.display = "grid"
+        this.location.reload();
+    })
+}
+
+function modalContentAdd(works) {
+    // Add modal content
+    //create modalAddBtn
+    if (!document.contains(document.querySelector(".modalAddBtn"))) {
+        let modalFooter = document.querySelector(".modalFooter")
+        let modalAddBtn = document.createElement("button")
+        modalAddBtn.className = "connect modalAddBtn"
+        modalAddBtn.innerHTML = "Ajouter une photo"
+        modalAddBtn.id = "modalIntialAdd"
+        modalFooter.appendChild(modalAddBtn)
+    } else {
+        let modalAddBtn = document.querySelector(".modalAddBtn")
+        modalAddBtn.innerHTML = "Ajouter une photo"
+        modalAddBtn.id = "modalIntialAdd"
+    }
+    //Add new work feature
+    let modalAddBtn = document.querySelector(".modalAddBtn")
+    modalAddBtn.addEventListener("click", () => {
+        modalAddInterface()
+    })
+
+    let modalGallery = document.querySelector(".modalBodyGallery");
+    let modalExistingWorks = modalGallery.querySelectorAll("figure");
+    let modalWorks = works;
 
     // Convert existingWorks NodeList to an array
     let modalExistingWorksArray = Array.from(modalExistingWorks);
-    let modalWorksArray = Array.from(modalWorks);
 
-    for (let i = 0; i < modalWorksArray.length; i++) {
-        let modalNewWork = modalWorksArray[i];
+    for (let i = 0; i < modalWorks.length; i++) {
+        let modalNewWork = modalWorks[i];
 
         // Check if the work with the same title already exists
         let modalWorkAlreadyExists = modalExistingWorksArray.some(modalExistingWork => {
             let modalExistingAlt = modalExistingWork.querySelector("img").alt;
-            let modalNewAlt = modalNewWork.querySelector("img").alt;
+            let modalNewAlt = modalNewWork.title;
             return modalExistingAlt === modalNewAlt;
         });
 
         if (!modalWorkAlreadyExists) {
             // Create new figure element for each work and append to the gallery
+            let token = getCookie("connectionCookie");
             let modalNewFigure = document.createElement("figure");
+            modalNewFigure.id = modalNewWork.id
             let modalNewImg = document.createElement("img");
-            modalNewImg.src = modalNewWork.querySelector("img").currentSrc;
-            modalNewImg.alt = modalNewWork.querySelector("img").alt;
+            modalNewImg.src = modalNewWork.imageUrl;
+            modalNewImg.alt = modalNewWork.title;
 
             modalNewFigure.appendChild(modalNewImg);
 
+            //create image delete feature
+            let modalDeleteBtn = document.createElement("button")
+            modalDeleteBtn.className = "connect modalDeleteBtn"
+            modalDeleteBtn.id = modalNewWork.id
+            modalDeleteBtn.innerHTML = '<i class="fa-solid fa-trash-can fa-2xs"></i>'
+            modalNewFigure.appendChild(modalDeleteBtn)    
+
             modalGallery.appendChild(modalNewFigure);
+            modalDeleteBtn.addEventListener("click", async () => {
+                try {
+                    let deleteId = modalDeleteBtn.id
+                    let deletePayload = "Bearer " + token 
+                    console.log(deletePayload)
+                    let deleteAction = await fetch("http://localhost:5678/api/works/"+ deleteId, {
+                        method: "DELETE",
+                        headers: {accept: "*/*", Authorization: deletePayload },
+                    });
+                    let deleteResponse = await deleteAction.json();
+                    console.log(deleteResponse)
+                    if (deleteResponse.message === 'Item Deleted') {
+                        let deleteFigure = document.getElementById(deleteId)
+                        deleteFigure.remove()
+                    }
+                } catch(error) {
+                    console.log("Error while removing work : " + error)
+                }
+            })
             console.log("Modal content added");
         } else {
             console.log("Modal: Work with title already exists");
         }
     }
+}
+
+function modalAddInterface() {
+    // create add interface
+    // Do not display gallery
+    let modalGallery = document.querySelector(".modalBodyGallery");
+    modalGallery.style.display = "none";
+    
+    let modalBodyTxt = document.querySelector(".modalBodyTxt")
+    modalBodyTxt.innerHTML = "Ajout photo"
+    
+    let modalAdd = document.querySelector(".modalBodyAddDiv")
+    modalAdd.style.display = "flex"
+    
 }
 
 
